2e6aa51f01339172b0c09c50fc09d6064353c7d2
salmanhabib220 <habibsalman220@gmail.com>
2022-03-25 20:01:37 +0500

    Assignment No 01
    This is My First Assignment which is about Layout

---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Setting up theme
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('UI Layout'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: const [
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.apple,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.android,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.car_repair,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.check_box,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.power_off_outlined,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.light_mode,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Icon(
+                        Icons.lightbulb,
+                        color: Colors.grey,
+                        size: 100,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Listing down operating systems
+            Expanded(
+              flex: 4,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: const [
+                    Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.android),
+                    title: Text('Android'),
+                    subtitle: Text(
+                        'Android is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.apple),
+                    title: Text('iOS'),
+                    subtitle: Text(
+                        'iOS is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                  Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.gif_box),
+                    title: Text('Windows'),
+                    subtitle: Text(
+                        'Windows is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                         Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.android),
+                    title: Text('Android'),
+                    subtitle: Text(
+                        'Android is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                  Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.apple),
+                    title: Text('iOS'),
+                    subtitle: Text(
+                        'iOS is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                        Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.gif_box),
+                    title: Text('Windows'),
+                    subtitle: Text(
+                        'Windows is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                        Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.android),
+                    title: Text('Android'),
+                    subtitle: Text(
+                        'Android is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                      Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.apple),
+                    title: Text('iOS'),
+                    subtitle: Text(
+                        'iOS is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: ListTile(
+                    leading: Icon(Icons.gif_box),
+                    title: Text('Windows'),
+                    subtitle: Text(
+                        'Windows is an operating system for mobile devices such as phones and tablets.'),
+                  ),
+                ),
+                
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
